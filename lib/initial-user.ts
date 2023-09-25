470766ef9682d4effe09298fddeb3d54edae5032
ECarry <lianshiliang93@gmail.com>
2023-09-25 23:39:24 +0800

    feat: init user

---
+++ lib/initial-user.ts
@@ -0,0 +1,31 @@
+import { currentUser, redirectToSignIn } from '@clerk/nextjs'
+import { db } from '@/lib/db'
+
+export const initialUser = async () => {
+  const user = await currentUser()
+
+  if (!user) {
+    return redirectToSignIn()
+  }
+
+  const findUser = await db.user.findFirst({
+    where: {
+      userId: user.id
+    }
+  })
+
+  if (findUser) {
+    return findUser
+  }
+
+  const newUser = await db.user.create({
+    data: {
+      userId: user.id,
+      imageUrl: user.imageUrl,
+      email: user.emailAddresses[0].emailAddress,
+      name: `${user.firstName} ${user.lastName}`,
+    }
+  })
+
+  return newUser
+}
