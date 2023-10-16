90ce33dadb1a243c00a6f18c00b35ea85331a584
ECarry <lianshiliang93@gmail.com>
2023-10-17 00:01:34 +0800

    feat: Navbar

--- app/(home)/_components/Navbar.tsx
+++ app/(home)/_components/Navbar.tsx
@@ -1,29 +1,45 @@
+import Logo from '@/components/Logo'
 import Icon from '@/components/icons'
+import Link from 'next/link'
 import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className='flex'>
-      <Icon 
-        name='github'
-        animated='HOVER'
-        loop
-        size={28}
-      />
+    <div className='z-50 w-full'>
+      <div className='flex items-center'>
+        {/* LOGO  */}
+        <Link href='/'>
+          <Logo />
+        </Link>
 
-      <Icon 
-        name='twitter'
-        animated='HOVER'
-        loop
-        size={28}
-      />
+        {/* NAV  */}
 
-      <Icon 
-        name='instagram'
-        animated='HOVER'
-        loop={false}
-        size={28}
-      />
+        {/* ICON  */}
+        <div className='flex items-center gap-2'>
+          <Icon 
+          name='github'
+          animated='HOVER'
+          loop
+          size={28}
+          />
+
+          <Icon 
+            name='twitter'
+            animated='HOVER'
+            loop
+            size={28}
+          />
+
+          <Icon 
+            name='instagram'
+            animated='HOVER'
+            loop={false}
+            size={28}
+          />
+        </div>
+      </div>
+
+      
     </div>
   )
 }
