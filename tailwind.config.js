5f40d3a459fefd5373552d30a192b46c07ab27d3
ECarry <lianshiliang93@gmail.com>
2023-10-17 23:39:51 +0800

    fix: uploadthing styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+const { withUt } = require("uploadthing/tw");
+
+module.exports = withUt({
   darkMode: ["class"],
   content: [
     './pages/**/*.{ts,tsx}',
@@ -77,4 +79,4 @@ module.exports = {
     require('@tailwindcss/aspect-ratio'),
   ],
   
-}
+})
