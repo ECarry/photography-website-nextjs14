22e68f19b2cf11c2ea6f409df4df0739230a9d78
ecarry <lianshiliang93@gmail.com>
2023-10-18 09:11:06 +0800

    feat:  Dashboard Meta

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,6 +1,12 @@
+import type { Metadata } from 'next'
 import { initialUser } from "@/lib/initial-user"
 import PageHeader from "../../_components/PageHeader";
 import PhotoGallery from "@/components/PhotoGallery";
+
+export const metadata: Metadata = {
+  title: 'Dashboard - ECarry Photography',
+  description: 'Dashboard',
+}
 
 const page = async () => {
   const user = await initialUser()
