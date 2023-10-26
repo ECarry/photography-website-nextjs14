03c866c1dafcc43412298b907457710823194b1e
ecarry <lianshiliang93@gmail.com>
2023-10-26 14:16:14 +0800

    feat: album id page

--- app/(dashboard)/(routes)/albums/(routes)/[albumId]/page.tsx
+++ app/(dashboard)/(routes)/albums/(routes)/[albumId]/page.tsx
@@ -1,5 +1,9 @@
 import { db } from "@/lib/db";
 import { Metadata } from "next";
+import Image from "next/image";
+
+import PageHeader from "@/app/(dashboard)/_components/PageHeader";
+import { AspectRatio } from "@/components/ui/aspect-ratio";
 
 interface AlbumIdPageProps {
   params: {
@@ -12,23 +16,45 @@ export async function generateMetadata(
 ): Promise<Metadata> {
   const { albumId } = params
 
-  const photo = await db.album.findFirst({
+  const album = await db.album.findFirst({
     where: {
       id: albumId
     }
   })
 
   return {
-    title: `Edit ${photo?.title} - Dashboard - ECarry Photography`
+    title: `Edit ${album?.title} - Dashboard - ECarry Photography`
   }
 }
 
-const AlbumIdPage = ({
+const AlbumIdPage = async ({
   params
 }: AlbumIdPageProps) => {
+  const album = await db.album.findFirst({
+    where: {
+      id: params.albumId
+    }
+  })
+
+  if (!album) {
+    return null
+  }
+
   return (
-    <div>
-      {params.albumId}
+    <div className="flex flex-col gap-4">
+      <AspectRatio ratio={16 / 10}>
+        <Image 
+          src={album?.imageUrl}
+          alt="album"
+          fill
+          className="object-cover"
+        />
+      </AspectRatio>
+      <PageHeader title="糖水日记"  label="Add Photo" type="createPhoto" />
+      
+      <h1>{album?.title}</h1>
+      <h2>{params.albumId}</h2>
+      <p>{album?.description}</p>
     </div>
   )
 }
