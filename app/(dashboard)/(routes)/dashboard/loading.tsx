ac675ad06e0907fe64d8f879489173a0070db1d2
ecarry <lianshiliang93@gmail.com>
2023-10-27 16:00:53 +0800

    fix: The loading skeleton bug with route groups

--- app/(dashboard)/(routes)/dashboard/loading.tsx
+++ app/(dashboard)/(routes)/dashboard/loading.tsx
@@ -1,4 +1,4 @@
-import DashboardSkeleton from "./_components/skeletons";
+import DashboardSkeleton from "../../_components/skeletons";
 
 export default function Loading() {
   return <DashboardSkeleton />;
