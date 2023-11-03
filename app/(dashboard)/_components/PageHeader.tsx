21734701bb5693bc4c228a9f124d587bb12415e9
ecarry <lianshiliang93@gmail.com>
2023-11-03 15:07:01 +0800

    style: page header sytle

--- app/(dashboard)/_components/PageHeader.tsx
+++ app/(dashboard)/_components/PageHeader.tsx
@@ -1,19 +1,22 @@
 'use client'
 
 import { Button } from "@/components/ui/button"
+import { Separator } from "@/components/ui/separator";
 import { useModal } from "@/hooks/use-modal-store";
 import { Plus } from "lucide-react";
 
 interface PageHeaderProps {
   title: string;
-  label: string;
+  desc?: string;
+  label?: string;
   id?: string;
-  type: 'createPhoto' | 'createAlbum';
+  type?: 'createPhoto' | 'createAlbum';
   showButton?: boolean;
 }
 
 const PageHeader = ({
   title,
+  desc,
   label,
   type,
   id,
@@ -26,21 +29,28 @@ const PageHeader = ({
   }
 
   return (
-    <div className="flex items-center justify-between">
-      <h1 className="text-2xl md:text-4xl font-semibold">
-        {title}
-      </h1>
-      {showButton && (
-        <Button
-        onClick={() => onOpen(type, data)}
-        variant='primary'
-      >
-        <Plus size={16} className="mr-2" />
-        {label}
-      </Button>
-      )}
-      
-    </div>
+    <>
+      <div className="flex items-center justify-between">
+        <div className="space-y-0.5">
+          <h2 className="text-2xl font-bold tracking-tight">{title}</h2>
+          <p className="text-muted-foreground">
+            {desc}
+          </p>
+        </div>
+        
+        {showButton && type && (
+          <Button
+          onClick={() => onOpen(type, data)}
+          variant='primary'
+        >
+          <Plus size={16} className="mr-2" />
+          {label}
+        </Button>
+        )}
+        
+      </div>
+      <Separator className="my-6" />
+    </>
   )
 }
 
