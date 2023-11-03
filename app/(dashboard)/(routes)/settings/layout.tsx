a43cde16d22cd34900cb2dc7b7afcb2d4807a835
ecarry <lianshiliang93@gmail.com>
2023-11-03 16:37:44 +0800

    style: settings layout style

--- app/(dashboard)/(routes)/settings/layout.tsx
+++ app/(dashboard)/(routes)/settings/layout.tsx
@@ -33,7 +33,7 @@ export default function SettingsLayout({ children }: SettingsLayoutProps) {
           showButton={false}
         />
         <div className="flex flex-col space-y-8 lg:flex-row lg:space-x-12 lg:space-y-0">
-          <aside className=" lg:w-1/5">
+          <aside className="lg:w-1/5">
             <SidebarNav items={sidebarNavItems} />
           </aside>
           <div className="flex-1 lg:max-w-2xl">{children}</div>
