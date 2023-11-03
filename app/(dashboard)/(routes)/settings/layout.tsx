15f50fd4df453749dc673e77eb5cb876f6f2b305
ecarry <lianshiliang93@gmail.com>
2023-11-03 15:06:25 +0800

    feat: settings page

---
+++ app/(dashboard)/(routes)/settings/layout.tsx
@@ -0,0 +1,44 @@
+import { Metadata } from "next"
+
+import { SidebarNav } from "./_components/sidebar-nav"
+import PageHeader from "../../_components/PageHeader"
+
+export const metadata: Metadata = {
+  title: "Settings - ECarry Photography",
+  description: "Settings",
+}
+
+const sidebarNavItems = [
+  {
+    title: "Profile",
+    href: "/settings/profile",
+  },
+  {
+    title: "Categary",
+    href: "/settings/categary",
+  },
+]
+
+interface SettingsLayoutProps {
+  children: React.ReactNode
+}
+
+export default function SettingsLayout({ children }: SettingsLayoutProps) {
+  return (
+    <>
+      <div className="hidden space-y-6 pb-16 md:block">
+        <PageHeader
+          title="Settings"
+          desc="Manage your account settings and set e-mail preferences."
+          showButton={false}
+        />
+        <div className="flex flex-col space-y-8 lg:flex-row lg:space-x-12 lg:space-y-0">
+          <aside className="-mx-4 lg:w-1/5">
+            <SidebarNav items={sidebarNavItems} />
+          </aside>
+          <div className="flex-1 lg:max-w-2xl">{children}</div>
+        </div>
+      </div>
+    </>
+  )
+}
