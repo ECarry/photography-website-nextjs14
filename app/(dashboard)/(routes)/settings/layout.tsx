29b78dd197682c4ea33d2a7e0d9b3293619ae189
ecarry <lianshiliang93@gmail.com>
2023-11-03 16:37:15 +0800

    style: layout style

--- app/(dashboard)/(routes)/settings/layout.tsx
+++ app/(dashboard)/(routes)/settings/layout.tsx
@@ -26,14 +26,14 @@ interface SettingsLayoutProps {
 export default function SettingsLayout({ children }: SettingsLayoutProps) {
   return (
     <>
-      <div className="hidden space-y-6 pb-16 md:block">
+      <div className="space-y-6 pb-16">
         <PageHeader
           title="Settings"
           desc="Manage your account settings and set e-mail preferences."
           showButton={false}
         />
         <div className="flex flex-col space-y-8 lg:flex-row lg:space-x-12 lg:space-y-0">
-          <aside className="-mx-4 lg:w-1/5">
+          <aside className=" lg:w-1/5">
             <SidebarNav items={sidebarNavItems} />
           </aside>
           <div className="flex-1 lg:max-w-2xl">{children}</div>
