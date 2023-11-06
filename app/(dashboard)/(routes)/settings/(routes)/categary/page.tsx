fbb2e84bbe0153c03c4a9271ba1c3be567e5a20f
ecarry <lianshiliang93@gmail.com>
2023-11-06 09:49:35 +0800

    feat: settings categary pageee

---
+++ app/(dashboard)/(routes)/settings/(routes)/categary/page.tsx
@@ -0,0 +1,18 @@
+import { Separator } from '@/components/ui/separator'
+
+const SettingsCategaryPage = () => {
+  return (
+    <div className="space-y-6">
+    <div>
+      <h3 className="text-lg font-medium">Categary</h3>
+      <p className="text-sm text-muted-foreground">
+        This is how others will see you on the site.
+      </p>
+    </div>
+    <Separator />
+
+  </div>
+  )
+}
+
+export default SettingsCategaryPage
