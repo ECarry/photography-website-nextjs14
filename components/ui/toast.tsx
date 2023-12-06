7e2f51a8622c075b536034b023f9deda593090dd
ecarry <lianshiliang93@gmail.com>
2023-12-06 14:57:56 +0800

    style: toast green bg

--- components/ui/toast.tsx
+++ components/ui/toast.tsx
@@ -30,6 +30,7 @@ const toastVariants = cva(
         default: "border bg-background text-foreground",
         destructive:
           "destructive group border-destructive bg-destructive text-destructive-foreground",
+        successful: 'border bg-background text-foreground bg-green-300'
       },
     },
     defaultVariants: {
