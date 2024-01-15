e1a22ee20a39268d2677c00ebce625c3b793322c
ECarry <lianshiliang93@gmail.com>
2024-01-15 21:36:09 +0800

    feat: photo page info & ctrl

--- app/(home)/(routes)/p/[id]/page.tsx
+++ app/(home)/(routes)/p/[id]/page.tsx
@@ -1,5 +1,6 @@
 import { fetchPhotoInfo } from "@/lib/data"
 import Image from "next/image"
+import Link from "next/link"
 
 interface PhotoIdPageProps {
   params: {
@@ -27,6 +28,20 @@ const PhotoIdPage = async ({
         height={data.width}
         className="max-h-full max-w-full object-contain h-full w-full" 
       />
+
+      <div className="absolute left-[50px] bottom-[50px] w-[180px]">
+        <div className="mb-[10px] text-sm">
+          <h1 className="font-bold mb-[10px]">{data.title}</h1>
+          <p className="text-left">{data.description}</p>
+        </div>
+        <div className="text-xs flex gap-1">
+          <span className="text-[#222]">PREV</span>
+          <span className="text-[#aaa]">/</span>
+          <span className="text-[#222]">NEXT</span>
+        </div>
+
+        <Link href='/grid' className="uppercase text-xs text-[#666666]">show thumbnails</Link>
+      </div>
     </div>
   )
 }
