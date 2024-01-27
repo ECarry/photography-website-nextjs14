9058e9c617397b4c0977cf6b8a6c115c05c98d3c
ECarry <lianshiliang93@gmail.com>
2024-01-27 14:37:22 +0800

    fix: md screen sidebar

--- app/(home)/(routes)/fan/page.tsx
+++ app/(home)/(routes)/fan/page.tsx
@@ -6,7 +6,7 @@ const page = async () => {
   const albums = await fetchAlbum()
 
   return (
-    <div className='flex items-center justify-center flex-col h-screen ml-[280px]'>
+    <div className='flex items-center justify-center flex-col h-screen ml-0 md:ml-[280px]'>
       <InfiniteMovingCards
         direction="left"
         speed='slow'
