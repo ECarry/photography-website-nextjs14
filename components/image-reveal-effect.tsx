9a26ac4fbb2dd6ccafc243a2284a28969263f4b6
ECarry <lianshiliang93@gmail.com>
2024-01-31 21:51:11 +0800

    update animate

--- components/image-reveal-effect.tsx
+++ components/image-reveal-effect.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import { useState } from "react";
-import MotionDiv from "./motion-div";
 import Image from "next/image"
 import { Photo } from "@prisma/client";
 import { motion } from "framer-motion";
@@ -10,38 +9,31 @@ interface ImageRevealEffectProps {
   photo: Photo
 }
 
-const hiddenMask = `repeating-linear-gradient(to right, rgba(0,0,0,0) 0px, rgba(0,0,0,0) 30px, rgba(0,0,0,1) 30px, rgba(0,0,0,1) 30px)`;
-const visibleMask = `repeating-linear-gradient(to right, rgba(0,0,0,0) 0px, rgba(0,0,0,0) 0px, rgba(0,0,0,1) 0px, rgba(0,0,0,1) 30px)`;
+const loadingVariants = {
+  hidden: { opacity: 0, y: 10 },
+  visible: { opacity: 1, y: 0 },
+};
 
+const imageVariants = {
+  hidden: { opacity: 0, y: 10 },
+  visible: { opacity: 1, y: 0 },
+};
 
 const ImageRevealEffect = ({
   photo
 }: ImageRevealEffectProps) => {
   const [isLoaded, setIsLoaded] = useState(false);
   const [isInView, setIsInView] = useState(false);
-  
+
   return (
     <motion.div
       className='w-full h-full'
-      // initial={{ y: 10, opacity: 0 }}
-      // animate={
-      //   isLoaded && isInView
-      //     ? { y: 0, opacity: 1 }
-      //     : { y: 0, opacity: 1 }
-      // }
-      // viewport={{ once: true }}
-      // onViewportEnter={() => setIsInView(true)}
-      // exit={{ y: -10, opacity: 0 }}
-      // transition={{ duration: 0.75 }}
-      initial={false}
-        animate={
-          isLoaded && isInView
-            ? { WebkitMaskImage: visibleMask, maskImage: visibleMask }
-            : { WebkitMaskImage: hiddenMask, maskImage: hiddenMask }
-        }
-        transition={{ duration: 1, delay: 1 }}
-        viewport={{ once: true }}
-        onViewportEnter={() => setIsInView(true)}
+      initial={{ y: 10, opacity: 0 }}
+      animate={isLoaded && isInView ? "visible" : "hidden"}
+      variants={isLoaded ? imageVariants : loadingVariants}
+      onViewportEnter={() => setIsInView(true)}
+      exit={{ y: -10, opacity: 0 }}
+      transition={{ duration: 0.75 }}
     >
       <Image 
         src={photo.imageUrl}
