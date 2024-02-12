8aa20d4b5433c94ac7211cd775e42b181f13ebd7
ECarry <lianshiliang93@gmail.com>
2024-02-12 23:13:02 +0800

    style: login image style

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -24,14 +24,14 @@ const LoginPage = async () => {
 
   return (
     <main className='w-full h-screen flex flex-col lg:flex-row'>
-      <div className='flex-1 overflow-hidden'>
+      <div className='flex-1 overflow-hidden p-4 md:p-8'>
         <Image 
           src={imageUrl}
           alt='login image'
           priority
           width={1920}
           height={1080}
-          className='object-cover w-full h-full'
+          className='object-cover w-full h-full rounded-lg md:rounded-3xl'
         />
       </div>
 
