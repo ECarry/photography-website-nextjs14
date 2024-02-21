36ddb6efe1469f4fb2e7dce5aee3857aff080685
ecarry <lianshiliang93@gmail.com>
2024-02-21 16:50:07 +0800

    Vercel Web Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,32 +1,33 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { IBM_Plex_Mono } from 'next/font/google'
+import "./globals.css";
+import type { Metadata } from "next";
+import { IBM_Plex_Mono } from "next/font/google";
 
-import { MoadlProvider } from '@/components/providers/ModalProvider'
-import { Toaster } from "@/components/ui/toaster"
-import { ThemeProvider } from "@/components/providers/theme-provider"
-import AuthProvider from '@/components/providers/auth-provider'
-import { SpeedInsights } from "@vercel/speed-insights/next"
-import { TailwindIndicator } from '@/components/tailwind-indicator'
+import { MoadlProvider } from "@/components/providers/ModalProvider";
+import { Toaster } from "@/components/ui/toaster";
+import { ThemeProvider } from "@/components/providers/theme-provider";
+import AuthProvider from "@/components/providers/auth-provider";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { TailwindIndicator } from "@/components/tailwind-indicator";
+import { Analytics } from "@vercel/analytics/react";
 
 const ibmPlexMono = IBM_Plex_Mono({
-  subsets: ['latin'],
-  weight: ['400', '700'],
-  variable: '--font-ibm-plex-mono',
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  variable: "--font-ibm-plex-mono",
 });
 
 export const metadata: Metadata = {
   title: {
-    template: '%s - ECarry Photography',
-    default: 'ECarry Photography',
+    template: "%s - ECarry Photography",
+    default: "ECarry Photography",
   },
-  description: 'ECarry Photography',
-}
+  description: "ECarry Photography",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
@@ -34,6 +35,7 @@ export default function RootLayout({
         <AuthProvider>
           <ThemeProvider attribute="class" defaultTheme="system">
             {children}
+            <Analytics />
             <TailwindIndicator />
             <MoadlProvider />
             <Toaster />
@@ -42,5 +44,5 @@ export default function RootLayout({
         </AuthProvider>
       </body>
     </html>
-  )
+  );
 }
