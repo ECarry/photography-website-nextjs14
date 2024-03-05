043b988f7aa6380e6c6297c0cba5c889e4eb2b9b
ecarry <lianshiliang93@gmail.com>
2024-03-05 13:56:44 +0800

    style: code style

--- components/modals/CreatePhotoModal.tsx
+++ components/modals/CreatePhotoModal.tsx
@@ -1,132 +1,143 @@
-import { useModal } from '@/hooks/use-modal-store'
-import { useRouter } from 'next/navigation'
-import { useState, useTransition } from 'react'
-import { createPhoto } from '@/actions/createPhoto'
-import { getExifData } from '@/lib/getExifData'
-import { getImageSize } from '@/lib/getImageSize'
-import { uploadFiles } from '@/actions/uploadPhoto'
-import extractExifData from '@/lib/extractExifData'
-import { UploadFileResponse } from '@/types'
+import { useModal } from "@/hooks/use-modal-store";
+import { useRouter } from "next/navigation";
+import { useState, useTransition } from "react";
+import { createPhoto } from "@/actions/createPhoto";
+import { getExifData } from "@/lib/getExifData";
+import { getImageSize } from "@/lib/getImageSize";
+import { uploadFiles } from "@/actions/uploadPhoto";
+import extractExifData from "@/lib/extractExifData";
+import { UploadFileResponse } from "@/types";
 
-import { 
-  Dialog, 
-  DialogContent, 
-  DialogDescription, 
-  DialogFooter, 
-  DialogHeader, 
-  DialogTitle,  
-} from "@/components/ui/dialog"
-import { Button } from '@/components/ui/button'
-import FormError from '@/components/form-error'
-import FormSuccess from '@/components/form-success'
-import ImagePreview from '@/components/image-preview'
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
+import FormError from "@/components/form-error";
+import FormSuccess from "@/components/form-success";
+import ImagePreview from "@/components/image-preview";
 
 const CreatePhotoModal = () => {
-  const [isPending, startTransition] = useTransition()
-  const [error, setError] = useState<string | undefined>()
-  const [success, setSuccess] = useState<string | undefined>()
-  const [images, setImages] = useState<UploadFileResponse[] >()
+  const [isPending, startTransition] = useTransition();
+  const [error, setError] = useState<string | undefined>();
+  const [success, setSuccess] = useState<string | undefined>();
+  const [images, setImages] = useState<UploadFileResponse[]>();
 
-  const [formData, setFormData] = useState<any | undefined>({})
+  const [formData, setFormData] = useState<any | undefined>({});
 
-  const router = useRouter()
+  const router = useRouter();
 
-  const { onClose, isOpen, type, data } = useModal()
-  const isModalOpen = isOpen && type === 'createPhoto'
+  const { onClose, isOpen, type } = useModal();
+  const isModalOpen = isOpen && type === "createPhoto";
 
   const handleImageChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
-  const file = e.target.files?.[0];
-  if (!file) return;
+    const file = e.target.files?.[0];
+    if (!file) return;
 
-  const [size, exif] = await Promise.all([
+    const [size, exif] = await Promise.all([
       getImageSize(file),
-      getExifData(file)
+      getExifData(file),
     ]);
 
-  if(!exif) {
-    setError('Exif does not found.')
-  }
+    console.log(exif);
 
-  setFormData((prevList: any) => ({ ...prevList, ...size, ...(exif && { ...extractExifData(exif) }) }));
-};
+    setFormData((prevList: any) => ({
+      ...prevList,
+      ...size,
+      ...(exif && { ...extractExifData(exif) }),
+    }));
+  };
 
   const onUploadSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    setSuccess("");
+    setError("");
     e.preventDefault();
 
     const fd = new FormData(e.target as HTMLFormElement);
-    const uploadedFiles = await uploadFiles(fd);
+    startTransition(() => {
+      uploadFiles(fd).then((data) => {
+        //console.log(data[0]);
+        if (data[0].error !== null) {
+          return setError(data[0].error.message);
+        }
 
-    if (uploadedFiles[0].error !== null) {
-      return setError(uploadedFiles[0].error.message)
-    }
+        setSuccess("Files uploaded successfully");
 
-    setSuccess('Files uploaded successfully')
-
-    setImages(uploadedFiles);
-    setFormData((prevList: any) => ({ 
-    ...prevList,
-    title: uploadedFiles[0].data?.name,
-    imageUrl: uploadedFiles[0].data?.url,
-  }));
-    setSuccess('Files uploaded successfully');
-  }
+        setImages(data);
+        setFormData((prevList: any) => ({
+          ...prevList,
+          title: data[0].data?.name,
+          imageUrl: data[0].data?.url,
+        }));
+      });
+    });
+  };
 
   const onSubmit = async () => {
-    setError('')
-    setSuccess('')
-    const albumId = data.id ? data.id : null
+    setError("");
+    setSuccess("");
 
     startTransition(() => {
-      createPhoto(formData)
-      .then((data) => {
-        setError(data?.error)
-        setSuccess(data?.success)
-      })
-    })
-    
-    router.refresh()
-    onClose()
-  }
+      createPhoto(formData).then((data) => {
+        setError(data?.error);
+        setSuccess(data?.success);
+      });
+    });
+
+    router.refresh();
+    onClose();
+  };
 
   return (
     <Dialog open={isModalOpen} onOpenChange={onClose}>
       <DialogContent>
         <DialogHeader>
-          <DialogTitle className='text-center text-3xl'>
+          <DialogTitle className="text-center text-3xl">
             Upload Photo
           </DialogTitle>
-          <DialogDescription className='text-center'>
+          <DialogDescription className="text-center">
             Upload photo to Uploadthing
           </DialogDescription>
         </DialogHeader>
-        <div className='border-2 rounded-sm border-dashed py-10 px-4'>
-          <form
-            onSubmit={onUploadSubmit}
-          >
+        <div className="border-2 rounded-sm border-dashed py-10 px-4">
+          <form onSubmit={onUploadSubmit}>
             <input
-              name="files" 
-              type="file" 
-              multiple 
-              accept='image/*'
+              name="files"
+              type="file"
+              multiple
+              accept="image/*"
               onChange={handleImageChange}
+              disabled={isPending}
             />
-            <Button type='submit'>Upload</Button>
+            <Button type="submit" disabled={isPending}>
+              Upload
+            </Button>
           </form>
         </div>
 
-        {images?.map((image: UploadFileResponse, index: number) => ( 
-          <ImagePreview key={index} image={image} onChange={() => {
-            setSuccess('')
-            setImages(undefined)
-          }} />
+        {images?.map((image: UploadFileResponse, index: number) => (
+          <ImagePreview
+            key={index}
+            image={image}
+            onChange={() => {
+              setSuccess("");
+              setImages(undefined);
+            }}
+          />
         ))}
 
         <FormError message={error} />
         <FormSuccess message={success} />
-        <Button onClick={() => onSubmit()}>Create</Button>
+        <Button onClick={() => onSubmit()} disabled={isPending}>
+          Create
+        </Button>
       </DialogContent>
     </Dialog>
-  )
-}
+  );
+};
 
-export default CreatePhotoModal
+export default CreatePhotoModal;
--- components/photo/PhotoLarge.tsx
+++ components/photo/PhotoLarge.tsx
@@ -1,31 +1,32 @@
-import React from 'react'
-import Link from 'next/link'
-import { cn } from '@/lib/utils'
-import { Photo } from '@prisma/client'
-import formatCustomDate from '@/lib/formatCustomDate'
+import React from "react";
+import Link from "next/link";
+import { cn } from "@/lib/utils";
+import { Photo } from "@prisma/client";
+import formatCustomDate from "@/lib/formatCustomDate";
 
-import SiteGrid from '@/components/SiteGrid'
-import ImageLarge from '@/components/ImageLarge'
-import convertCoordinates from '@/lib/convertCoordinateToPoint'
+import SiteGrid from "@/components/SiteGrid";
+import ImageLarge from "@/components/ImageLarge";
+import convertCoordinates from "@/lib/convertCoordinateToPoint";
 
-const PhotoLarge =
-({
+const PhotoLarge = ({
   photo,
-  priority
+  priority,
 }: {
-  photo: Photo
-  priority?: boolean
+  photo: Photo;
+  priority?: boolean;
 }) => {
-
-  const renderMiniGrid = (children: JSX.Element) =>
-    <div className={cn(
-      'flex gap-y-4',
-      'flex-col sm:flex-row lg:flex-col',
-      '[&>*]:sm:flex-grow',
-      'pr-2',
-    )}>
+  const renderMiniGrid = (children: JSX.Element) => (
+    <div
+      className={cn(
+        "flex gap-y-4",
+        "flex-col sm:flex-row lg:flex-col",
+        "[&>*]:sm:flex-grow",
+        "pr-2"
+      )}
+    >
       {children}
-    </div>;
+    </div>
+  );
 
   return (
     <SiteGrid
@@ -37,68 +38,75 @@ const PhotoLarge =
           aspectRatio={photo.aspectRatio}
           priority={priority}
           id={photo.id}
-        />}
+        />
+      }
       contentSide={
-        <div className={cn(
-          'sticky top-4 self-start text-sm',
-          'grid grid-cols-2 lg:grid-cols-1',
-          'gap-y-4',
-          '-translate-y-1',
-          'mb-4',
-        )}>
-          {renderMiniGrid(<>
-            {/* TITLT  */}
-            <Link
-              href={`/p/${photo.id}`}
-              className="font-bold uppercase"
-            >
-              {photo.title}
-            </Link>
-            {/* CARMRA  */}
-            <div className="uppercase">
-              {photo.cameraMake} {photo.cameraModel}
-            </div>
-          </>)}
-          {renderMiniGrid(<>
-            <ul className={cn(
-              'text-gray-500',
-              'dark:text-gray-400',
-            )}>
-              <li>
-                {photo.focalLength}
-                {' '}
-                <span className={cn(
-                  'text-gray-400/80',
-                  'dark:text-gray-400/50',
-                )}>
-                  {photo.focalLengthIn35mmFilm}
-                </span>
-              </li>
-              <li>{photo.fNumber}</li>
-              <li>{photo.iso}</li>
-              <li>{photo.shutterSpeed}</li>
-              <li className='hidden lg:block'>{photo.latitude && photo.longitude ? convertCoordinates(photo.latitude, photo.longitude) : '-'}</li>
-              { photo.gpsAltitude ? 
-              <li className='hidden lg:block'>{photo.gpsAltitude}</li>
-              : null
-              }
-            </ul>
-            <div className={cn(
-              'flex gap-y-4',
-              'flex-col sm:flex-row lg:flex-col',
-            )}>
-              <div className={cn(
-                'grow uppercase',
-                'text-gray-500',
-                'dark:text-gray-400',
-              )}>
-                {photo.timestamp ? formatCustomDate(photo.timestamp) : '-'}
+        <div
+          className={cn(
+            "sticky top-4 self-start text-sm",
+            "grid grid-cols-2 lg:grid-cols-1",
+            "gap-y-4",
+            "-translate-y-1",
+            "mb-4"
+          )}
+        >
+          {renderMiniGrid(
+            <>
+              {/* TITLT  */}
+              <Link href={`/p/${photo.id}`} className="font-bold uppercase">
+                {photo.title}
+              </Link>
+              {/* CARMRA  */}
+              <div className="uppercase">
+                {photo.cameraMake} {photo.cameraModel}
               </div>
-            </div>
-          </>)}
-        </div>}
+            </>
+          )}
+          {renderMiniGrid(
+            <>
+              <ul className={cn("text-gray-500", "dark:text-gray-400")}>
+                <li>
+                  {photo.focalLength}{" "}
+                  <span
+                    className={cn("text-gray-400/80", "dark:text-gray-400/50")}
+                  >
+                    {photo.focalLengthIn35mmFilm}
+                  </span>
+                </li>
+                <li>{photo.fNumber}</li>
+                <li>{photo.iso}</li>
+                <li>{photo.shutterSpeed}</li>
+                <li className="hidden lg:block">
+                  {photo.latitude && photo.longitude
+                    ? convertCoordinates(photo.latitude, photo.longitude)
+                    : "-"}
+                </li>
+                {photo.gpsAltitude ? (
+                  <li className="hidden lg:block">{photo.gpsAltitude}</li>
+                ) : null}
+              </ul>
+              <div
+                className={cn(
+                  "flex gap-y-4",
+                  "flex-col sm:flex-row lg:flex-col"
+                )}
+              >
+                <div
+                  className={cn(
+                    "grow uppercase",
+                    "text-gray-500",
+                    "dark:text-gray-400"
+                  )}
+                >
+                  {photo.timestamp ? photo.timestamp : "-"}
+                </div>
+              </div>
+            </>
+          )}
+        </div>
+      }
     />
-  )
-}
+  );
+};
 
-export default PhotoLarge
+export default PhotoLarge;
