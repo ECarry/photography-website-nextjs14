2b12f9a7b5c8a86d925131b615770df311846d7e
ecarry <lianshiliang93@gmail.com>
2024-07-05 09:12:52 +0800

    test

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -2,11 +2,13 @@ import { Toaster } from "@/components/ui/sonner";
 import ModalProvider from "@/components/providers/modal-provider";
 import FloatMenu from "./_components/float-menu";
 import { QueryProvider } from "@/providers/QueryClientProvider";
+import Navbar from "./_components/Navbar";
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
       <QueryProvider>
+        <Navbar />
         <main className="pb-20">
           {children}
           <FloatMenu />
