55efc5a213cf58f1047fcf2ad418bd928a28add7
ecarry <lianshiliang93@gmail.com>
2024-07-11 16:39:55 +0800

    chore: settings page

--- app/(dashboard)/settings/page.tsx
+++ app/(dashboard)/settings/page.tsx
@@ -1,3 +1,5 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Pencil } from "lucide-react";
 import { Metadata } from "next";
 import React from "react";
 
@@ -6,7 +8,28 @@ export const metadata: Metadata = {
 };
 
 const SettingsPage = () => {
-  return <div>Coming soon...</div>;
+  return (
+    <div className="p-4 space-y-8">
+      <div className="space-y-2">
+        <h1 className="text-2xl font-bold">User account</h1>
+        <p className="text-muted-foreground text-sm">
+          Update your photo and personal details here.
+        </p>
+      </div>
+      <div className="flex items-center gap-8">
+        <Avatar className="size-20 cursor-pointer relative">
+          <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
+        <div className="space-y-1">
+          <h1 className="text-2xl font-semibold">ECarry</h1>
+          <p className="text-muted-foreground text-sm">
+            lianshiliang93@gmail.com
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SettingsPage;
