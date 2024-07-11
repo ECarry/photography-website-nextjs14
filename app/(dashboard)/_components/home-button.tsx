6a4206d8df00e011acfbd8e8344db7e5e8053782
ecarry <lianshiliang93@gmail.com>
2024-07-11 09:18:58 +0800

    chore: navbar button tooltip

--- app/(dashboard)/_components/home-button.tsx
+++ app/(dashboard)/_components/home-button.tsx
@@ -1,19 +1,34 @@
 import { Button } from "@/components/ui/button";
 import { ArrowUpRight } from "lucide-react";
 import Link from "next/link";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 
 const HomeButton = () => {
   return (
-    <Button
-      variant="outline"
-      size="icon"
-      className="rounded-full size-8"
-      asChild
-    >
-      <Link href="/">
-        <ArrowUpRight size={16} />
-      </Link>
-    </Button>
+    <TooltipProvider>
+      <Tooltip>
+        <TooltipTrigger>
+          <Button
+            variant="outline"
+            size="icon"
+            className="rounded-full size-8"
+            asChild
+          >
+            <Link href="/">
+              <ArrowUpRight size={16} />
+            </Link>
+          </Button>
+        </TooltipTrigger>
+        <TooltipContent align="end">
+          <p>Home</p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
   );
 };
 
--- app/(dashboard)/_components/upload-button.tsx
+++ app/(dashboard)/_components/upload-button.tsx
@@ -3,19 +3,34 @@
 import { Button } from "@/components/ui/button";
 import { useModal } from "@/hooks/use-modal";
 import { Upload } from "lucide-react";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 
 const UploadButton = () => {
   const { onOpen } = useModal();
 
   return (
-    <Button
-      onClick={() => onOpen()}
-      variant="outline"
-      size="icon"
-      className="rounded-full size-8"
-    >
-      <Upload size={16} />
-    </Button>
+    <TooltipProvider>
+      <Tooltip>
+        <TooltipTrigger>
+          <Button
+            onClick={() => onOpen()}
+            variant="outline"
+            size="icon"
+            className="rounded-full size-8"
+          >
+            <Upload size={16} />
+          </Button>
+        </TooltipTrigger>
+        <TooltipContent>
+          <p>New Photo</p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
   );
 };
 
