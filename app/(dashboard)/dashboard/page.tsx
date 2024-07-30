c467a4aa21d60bbc1dbbfce396e9301dae5b719a
ecarry <lianshiliang93@gmail.com>
2024-07-30 10:25:26 +0800

    style: geo map style

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -15,7 +15,7 @@ const page = () => {
       <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-4">
         <YearCountChart />
         <CityCountChart />
-        <div className="col-span-1 md:col-span-2 rounded-md h-[450px] overflow-hidden">
+        <div className="col-span-1 md:col-span-2 rounded-lg shadow-sm h-[450px] overflow-hidden border">
           <GeoMap />
         </div>
       </div>
