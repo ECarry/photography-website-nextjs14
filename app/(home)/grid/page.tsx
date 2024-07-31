d5ce1473a38d6f364d3593345f0e424e85e2bdbc
ecarry <lianshiliang93@gmail.com>
2024-07-31 15:53:14 +0800

    feat: gallery page

--- app/(home)/grid/page.tsx
+++ app/(home)/grid/page.tsx
@@ -1,7 +1,11 @@
-import React from "react";
+import PhotoGallery from "@/components/photo-gallery";
 
 const GridPage = () => {
-  return <section>Coming soon...</section>;
+  return (
+    <div className="p-4 md:py-[40px] md:pr-[50px] ml-0 md:ml-[280px]">
+      <PhotoGallery />
+    </div>
+  );
 };
 
 export default GridPage;
---
+++ components/render-next-image.tsx
@@ -0,0 +1,29 @@
+import Image from "next/image";
+import Link from "next/link";
+import { RenderImageContext, RenderImageProps } from "react-photo-album";
+
+export default function renderNextImage(
+  { alt = "", title, sizes }: RenderImageProps,
+  { photo, width, height }: RenderImageContext
+) {
+  return (
+    <div
+      style={{
+        width: "100%",
+        position: "relative",
+        aspectRatio: `${width} / ${height}`,
+      }}
+    >
+      <Image
+        fill
+        // width={width}
+        // height={height}
+        src={photo}
+        alt={alt}
+        title={title}
+        sizes={sizes}
+        placeholder={"blurDataURL" in photo ? "blur" : undefined}
+      />
+    </div>
+  );
+}
