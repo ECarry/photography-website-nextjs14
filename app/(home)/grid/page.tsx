665c368c5218af7109298233459a352fb51dbd6b
ecarry <lianshiliang93@gmail.com>
2024-09-10 09:16:06 +0800

    chore: page metadata

--- app/(home)/grid/page.tsx
+++ app/(home)/grid/page.tsx
@@ -1,5 +1,10 @@
 import PhotoGallery from "@/components/photo-gallery";
 
+// meta
+export const metadata = {
+  title: "Grid",
+};
+
 const GridPage = () => {
   return (
     <div className="p-4 md:py-[40px] md:pr-[50px] ml-0 md:ml-[280px]">
--- app/(home)/map/page.tsx
+++ app/(home)/map/page.tsx
@@ -1,5 +1,10 @@
 import Mapbox from "@/components/map";
 
+// meta
+export const metadata = {
+  title: "Map",
+};
+
 const MapPage = () => {
   return (
     <section className="ml-0 md:ml-[280px] h-dvh">
