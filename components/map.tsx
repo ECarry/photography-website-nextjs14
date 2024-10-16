6f1353b7207837e75d14cf8549cdbbf604b388e4
ecarry <lianshiliang93@gmail.com>
2024-10-16 15:22:53 +0800

    style: mark style

--- components/map.tsx
+++ components/map.tsx
@@ -66,8 +66,8 @@ const Mapbox = ({ showLocal = true }: Props) => {
           anchor="bottom"
         >
           <span className="relative flex h-3 w-3">
-            <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-sky-400 opacity-75"></span>
-            <span className="relative inline-flex rounded-full h-3 w-3 bg-sky-500"></span>
+            <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-rose-400 opacity-75"></span>
+            <span className="relative inline-flex rounded-full h-3 w-3 bg-rose-500"></span>
           </span>
         </Marker>
       )}
