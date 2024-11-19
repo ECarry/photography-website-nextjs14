e9c0b1de3105e188a593477273fc9044222e456a
ecarry <lianshiliang93@gmail.com>
2024-11-19 15:02:57 +0800

    style: photo card style update

--- app/(dashboard)/_components/photo-card.tsx
+++ app/(dashboard)/_components/photo-card.tsx
@@ -11,6 +11,10 @@ import { formatDate } from "@/lib/date";
 import Link from "next/link";
 import { useEditPhoto } from "@/features/photos/api/use-edit-photo";
 import { Icons } from "@/components/icons";
+import { useRouter } from "next/navigation";
+import { deleteCloudPhoto } from "@/actions/photos";
+import { useDeletePhoto } from "@/features/photos/api/use-delete-photo";
+import { useConfirm } from "@/hooks/use-confirm";
 
 export type Photo = InferResponseType<
   typeof client.api.photos.$get,
@@ -18,8 +22,15 @@ export type Photo = InferResponseType<
 >["data"][0];
 
 const PhotoCard = ({ photo }: { photo: Photo }) => {
+  const router = useRouter();
   const { map } = useMap();
   const editMutation = useEditPhoto(photo.id);
+  const deleteMutation = useDeletePhoto(photo.id);
+
+  const [ConfirmDialog, confirm] = useConfirm(
+    "Are you sure?",
+    "This action cannot be undone."
+  );
 
   const handlePhotoClick = () => {
     if (photo.longitude && photo.latitude) {
@@ -38,8 +49,22 @@ const PhotoCard = ({ photo }: { photo: Photo }) => {
     );
   };
 
+  const handleDelete = async () => {
+    const ok = await confirm();
+
+    if (ok) {
+      await deleteCloudPhoto(photo.url);
+      deleteMutation.mutate(undefined, {
+        onSuccess: async () => {
+          router.push("/photos");
+        },
+      });
+    }
+  };
+
   return (
     <div className="relative">
+      <ConfirmDialog />
       <AspectRatio
         ratio={4 / 5}
         className="bg-muted rounded-xl overflow-hidden"
@@ -58,35 +83,53 @@ const PhotoCard = ({ photo }: { photo: Photo }) => {
       <Button
         variant="outline"
         size="icon"
-        className="bg-white rounded-full absolute top-2 right-2 size-8"
+        className="backdrop-blur-sm bg-black/10 hover:bg-black/40 transition-all duration-150 border-0 rounded-full absolute top-4 right-4 size-12"
         onClick={handleHeartClick}
       >
         {photo.isFavorite ? (
           <Icons.heart
-            size={18}
+            size={22}
             className="text-muted-foreground fill-rose-500 text-rose-500"
           />
         ) : (
-          <Icons.heart size={18} className="text-muted-foreground" />
+          <Icons.heart size={22} className="text-muted-foreground text-white" />
         )}
       </Button>
 
-      <Link href={`/photos/${photo.id}`} className="group">
-        <div className="absolute bottom-0 left-0 w-full p-2">
-          <div className="w-full space-y-2 rounded-md overflow-hidden p-2 bg-white backdrop-blur supports-[backdrop-filter]:bg-background/75 group-hover:supports-[backdrop-filter]:bg-background/85 transition duration-150">
-            <h1 className="line-clamp-1 group-hover:underline">
-              {photo.title}
-            </h1>
-            <h3 className="text-xs text-muted-foreground">
-              {formatDate(photo.takeAt)}
-            </h3>
-            <p className="text-[10px] text-muted-foreground flex items-center font-light">
-              <Icons.mapPin size={12} className="text-sky-500 mr-2" />
-              {convertToCoordination(photo.longitude, photo.latitude)}
-            </p>
+      <div className="absolute bottom-0 left-0 w-full h-1/2">
+        <div className="w-full space-y-6 overflow-hidden p-4 transition duration-300 inset-x-0 bottom-0 h-full bg-gradient-to-t from-black to-black/0 flex flex-col justify-end">
+          <div className="space-y-2">
+            <h1 className="line-clamp-1 text-white text-lg">{photo.title}</h1>
+            <div className="flex justify-between">
+              <h3 className="text-xs text-white/70">
+                {formatDate(photo.takeAt)}
+              </h3>
+              <p className="text-[10px] text-white/70 flex items-center font-light">
+                <Icons.mapPin size={12} className="text-sky-500 mr-2" />
+                {convertToCoordination(photo.longitude, photo.latitude)}
+              </p>
+            </div>
+          </div>
+          <div className="flex w-full gap-x-4 mt-4">
+            <Button
+              onClick={() => router.push(`/photos/${photo.id}`)}
+              size="sm"
+              variant="secondary"
+              className="w-full"
+            >
+              Edit
+            </Button>
+
+            <Button
+              onClick={handleDelete}
+              size="sm"
+              className="w-full backdrop-blur bg-white/10 hover:bg-white/15"
+            >
+              Delete
+            </Button>
           </div>
         </div>
-      </Link>
+      </div>
     </div>
   );
 };
--- app/(dashboard)/_components/photo-list.tsx
+++ app/(dashboard)/_components/photo-list.tsx
@@ -27,7 +27,7 @@ const PhotoList = () => {
           <Icons.loader className="animate-spin" />
         </div>
       ) : (
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-4">
+        <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-2 2xl:grid-cols-3 gap-4">
           {photos.map((item) => (
             <PhotoCard key={item.id} photo={item} />
           ))}
