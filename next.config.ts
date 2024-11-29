4db6c86c709ee6094b121abde7a45e192c3a92d4
ecarry <lianshiliang93@gmail.com>
2024-11-29 22:01:49 +0800

    feat: R2 storage custom domain

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "photograph.ecarry.uk",
+        port: "",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
