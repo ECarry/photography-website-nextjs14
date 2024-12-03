0c59636fef67cb8958dd082b6978e39ba1a2e506
ecarry <lianshiliang93@gmail.com>
2024-12-03 17:35:56 +0800

    chore: use photos

--- src/app/(home)/_components/profile-card.tsx
+++ src/app/(home)/_components/profile-card.tsx
@@ -1,38 +1,41 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import Link from "next/link";
 import { PiArrowUpRight } from "react-icons/pi";
 
 const ProfileCard = () => {
   return (
-    <div className="flex flex-col p-10 gap-24 bg-muted hover:bg-muted-foreground transition-all duration-150 ease-[cubic-bezier(0.22, 1, 0.36, 1)] rounded-xl font-light cursor-pointer relative group flex-1">
-      <div className="flex gap-4 items-center">
-        {/* AVATAR  */}
-        <Avatar className="size-[60px]">
-          <AvatarImage
-            src="https://avatars.githubusercontent.com/u/16572906?s=400&u=a304af70d12572524d540553425d78ff4d1a101a&v=4"
-            alt="Avatar"
-          />
-          <AvatarFallback>EC</AvatarFallback>
-        </Avatar>
+    <Link href="/about">
+      <div className="flex flex-col p-10 gap-24 bg-muted hover:bg-muted-foreground transition-all duration-150 ease-[cubic-bezier(0.22, 1, 0.36, 1)] rounded-xl font-light relative group flex-1">
+        <div className="flex gap-4 items-center">
+          {/* AVATAR  */}
+          <Avatar className="size-[60px]">
+            <AvatarImage
+              src="https://avatars.githubusercontent.com/u/16572906?s=400&u=a304af70d12572524d540553425d78ff4d1a101a&v=4"
+              alt="Avatar"
+            />
+            <AvatarFallback>EC</AvatarFallback>
+          </Avatar>
 
-        {/* NAME  */}
-        <div className="flex flex-col gap-[2px]">
-          <h1 className="text-lg">ECarry</h1>
-          <p className="text-sm text-text-muted">Photographer</p>
+          {/* NAME  */}
+          <div className="flex flex-col gap-[2px]">
+            <h1 className="text-lg">ECarry</h1>
+            <p className="text-sm text-text-muted">Photographer</p>
+          </div>
+        </div>
+
+        <div>
+          <p className="text-text-muted text-[15px]">
+            I&apos;m ECarry, a photographer dedicated to capturing authentic
+            moments and telling stories through creative and emotional imagery,
+            wherever my journey takes me.
+          </p>
+        </div>
+
+        <div className="absolute top-8 right-8 opacity-0 group-hover:top-6 group-hover:right-6 group-hover:opacity-100 transition-all duration-300 ease-in-out">
+          <PiArrowUpRight size={18} />
         </div>
       </div>
-
-      <div>
-        <p className="text-text-muted text-[15px]">
-          I&apos;m ECarry, a photographer dedicated to capturing authentic
-          moments and telling stories through creative and emotional imagery,
-          wherever my journey takes me.
-        </p>
-      </div>
-
-      <div className="absolute top-8 right-8 opacity-0 group-hover:top-6 group-hover:right-6 group-hover:opacity-100 transition-all duration-300 ease-in-out">
-        <PiArrowUpRight size={18} />
-      </div>
-    </div>
+    </Link>
   );
 };
 
--- src/components/image-slider.tsx
+++ src/components/image-slider.tsx
@@ -1,10 +1,14 @@
 "use client";
 
 import Image from "next/image";
+import { useState } from "react";
 import Carousel from "./Carousel";
+import { cn } from "@/lib/utils";
+import { Blurhash } from "react-blurhash";
 import { useGetPhotos } from "@/features/photos/api/use-get-photos";
 
 export function ImageSlider() {
+  const [loadedImages, setLoadedImages] = useState<Record<string, boolean>>({});
   const { data: photos, isLoading, error } = useGetPhotos();
 
   if (isLoading) {
@@ -19,22 +23,53 @@ export function ImageSlider() {
     return <div>No photos available</div>;
   }
 
+  const handleImageLoad = (photoId: string) => {
+    setLoadedImages((prev) => ({
+      ...prev,
+      [photoId]: true,
+    }));
+  };
+
   return (
     <Carousel
       className="absolute top-0 left-0 w-full h-full rounded-xl"
       containerClassName="h-full"
     >
-      {photos.map((photo) => (
-        <div key={photo.id} className="flex-[0_0_100%] h-full relative">
-          <Image
-            src={photo.url}
-            alt={photo.title}
-            fill
-            className="object-cover"
-            quality={100}
-          />
-        </div>
-      ))}
+      {photos.map((photo) => {
+        return (
+          <div key={photo.id} className="flex-[0_0_100%] h-full relative">
+            {!loadedImages[photo.id] && (
+              <Blurhash
+                hash={photo.blurData}
+                width="100%"
+                height="100%"
+                resolutionX={32}
+                resolutionY={32}
+                punch={1}
+                className="absolute inset-0 z-10"
+              />
+            )}
+            <Image
+              src={photo.url}
+              alt={photo.title}
+              fill
+              sizes="(max-width: 640px) 100vw, 
+                     (max-width: 1080px) 75vw,
+                     (max-width: 1200px) 50vw,
+                     33vw"
+              quality={85}
+              priority={true}
+              loading="eager"
+              className={cn(
+                "object-cover z-20",
+                loadedImages[photo.id] ? "opacity-100" : "opacity-0"
+              )}
+              onLoad={() => handleImageLoad(photo.id)}
+              onLoadingComplete={() => handleImageLoad(photo.id)}
+            />
+          </div>
+        );
+      })}
     </Carousel>
   );
 }
