5ccfc146b4e5b0682ae07dd2756836347d19dded
ecarry <lianshiliang93@gmail.com>
2024-12-11 20:45:07 +0800

    feat: tiptap heading tools

--- src/app/(dashboard)/documents/[slug]/toolbar.tsx
+++ src/app/(dashboard)/documents/[slug]/toolbar.tsx
@@ -3,6 +3,7 @@
 import { Separator } from "@/components/ui/separator";
 import { useEditorStore } from "@/hooks/use-editor-store";
 import { cn } from "@/lib/utils";
+import { type Level } from "@tiptap/extension-heading";
 
 import {
   BoldIcon,
@@ -87,6 +88,90 @@ const FontFamilyButton = () => {
             }}
           >
             {font.label}
+          </DropdownMenuItem>
+        ))}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
+
+const HeadingLevelButton = () => {
+  const { editor } = useEditorStore();
+
+  const headingLevels = [
+    {
+      label: "Normal text",
+      value: 0,
+      fontSize: "16px",
+    },
+    {
+      label: "Heading 1",
+      value: 1,
+      fontSize: "32px",
+    },
+    {
+      label: "Heading 2",
+      value: 2,
+      fontSize: "24px",
+    },
+    {
+      label: "Heading 3",
+      value: 3,
+      fontSize: "20px",
+    },
+    {
+      label: "Heading 4",
+      value: 4,
+      fontSize: "18px",
+    },
+    {
+      label: "Heading 5",
+      value: 5,
+      fontSize: "16px",
+    },
+  ];
+
+  const getCurrentHeadingLevel = () => {
+    for (let level = 1; level <= 5; level++) {
+      if (editor?.isActive("heading", { level })) {
+        return `Heading ${level}`;
+      }
+    }
+
+    return "Normal text";
+  };
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <button className="text-sm h-7 w-[120px] shrink-0 flex items-center justify-between rounded-sm hover:bg-muted-hover px-1.5 overflow-hidden">
+          <span className="truncate">{getCurrentHeadingLevel()}</span>
+          <ChevronDownIcon className="ml-2 size-4 shrink-0" />
+        </button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="p-1 flex flex-col gap-y-1">
+        {headingLevels.map(({ label, value, fontSize }) => (
+          <DropdownMenuItem
+            key={value}
+            style={{ fontSize }}
+            onClick={() => {
+              if (value === 0) {
+                editor?.chain().focus().setParagraph().run();
+              } else {
+                editor
+                  ?.chain()
+                  .focus()
+                  .toggleHeading({ level: value as Level })
+                  .run();
+              }
+            }}
+            className={cn(
+              "flex items-center gap-x-2 px-2 py-1 rounded-sm",
+              value === 0 && !editor?.isActive("heading") && "bg-muted-hover",
+              editor?.isActive("heading", { level: value }) && "bg-muted-hover"
+            )}
+          >
+            {label}
           </DropdownMenuItem>
         ))}
       </DropdownMenuContent>
@@ -186,10 +271,9 @@ const Toolbar = () => {
         <ToolbarButton key={item.label} {...item} />
       ))}
       <Separator orientation="vertical" className="h-6 bg-muted-hover" />
-      {/* TODO: Font family */}
       <FontFamilyButton />
       <Separator orientation="vertical" className="h-6 bg-muted-hover" />
-      {/* TODO: Heading  */}
+      <HeadingLevelButton />
       <Separator orientation="vertical" className="h-6 bg-muted-hover" />
       {/* TODO: Font size */}
       <Separator orientation="vertical" className="h-6 bg-muted-hover" />
