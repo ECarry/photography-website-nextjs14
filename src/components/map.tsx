87ef6d713b0a255c4a1d95a6a5ab8587137429fc
ecarry <lianshiliang93@gmail.com>
2024-12-12 11:14:06 +0800

    style: map custom style

--- src/components/map.tsx
+++ src/components/map.tsx
@@ -42,8 +42,8 @@ export interface MapboxProps {
 }
 
 const MAP_STYLES = {
-  light: "mapbox://styles/mapbox/light-v11",
-  dark: "mapbox://styles/mapbox/dark-v11",
+  light: "mapbox://styles/ecarry/cldmhu6tr000001n33ujbxf7j",
+  dark: "mapbox://styles/ecarry/clp8hcmd300km01qx78rt0xaw",
 } as const;
 
 const Mapbox = ({
