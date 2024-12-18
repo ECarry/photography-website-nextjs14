b0ce682df6bebd36502fc9351ff922357842fc77
ecarry <lianshiliang93@gmail.com>
2024-12-18 09:34:16 +0800

    style: change logo icon

--- src/app/(home)/_components/header/logo.tsx
+++ src/app/(home)/_components/header/logo.tsx
@@ -1,11 +1,11 @@
-import { Camera } from "lucide-react";
-import AnimatedText from "../../../../components/animated-text";
 import Link from "next/link";
+import AnimatedText from "@/components/animated-text";
+import { RiCameraLensFill } from "react-icons/ri";
 
 const Logo = () => {
   return (
     <Link href="/" className="flex gap-2 items-center">
-      <Camera size={18} />
+      <RiCameraLensFill size={18} />
       <AnimatedText label="ECarry" label2="Photo" style="font-medium" />
     </Link>
   );
